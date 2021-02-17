072f52ea7f6f4c3cc90cef4aee675720260df17d
Julian Dans <gr8jld@gmail.com>
2021-02-16 20:38:38 -0800

    some stuff

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const Person = require('./db/Person');
 const app = express();
 const port = process.env.PORT || 3000
+const bp = require('body-parser');
+
+app.use(bp.urlencoded({ extended: false }))
+app.use(bp.json())
 
 app.get('/', (req,res) => {
   res.send("hallo")
