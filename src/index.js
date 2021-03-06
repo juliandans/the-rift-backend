c9d981f068286cc1af80885dfc7271f1015d14cb
Mitch Masia <mjmasia@gmail.com>
2021-03-06 11:02:53 -0600

    Enhances cookie life to 60 days.

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use(
   cookieSession({
     name: "the-rift-session",
     secret: process.env.SESSION_SECRET,
-    maxAge: 24 * 60 * 60 * 1000,
+    maxAge: 24 * 60 * 60 * 1000 * 60, // 60 days
     httpOnly: false,
   })
 );
